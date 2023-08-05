1fb90d5029789d99eec39d00c3354e4d9bcde178
rohitGupta2022 <pta.rohit28@gmail.com>
2023-08-06 00:09:49 +0530

    add: shadcn-button & folder str.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <p>Hello Admin Dashboard</p>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
