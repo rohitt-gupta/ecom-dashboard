eedc5e625f2c114c1439fcd22a67ac42f143eedf
rohitGupta2022 <pta.rohit28@gmail.com>
2023-08-08 20:06:51 +0530

    Add:- Store Modal Form
    Form Build added in side the very first model we use to create a store.
    Form uses React-hook-form and Zod for validation

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,41 @@
-'use client'
-import React from 'react'
-import { Modal } from '@/components/ui/modal'
-import { useStoreModal } from '@/hooks/use-store-modal'
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    // TODO : Create Store
+  };
+
   return (
     <Modal
       title='Create store'
@@ -12,9 +43,35 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Future Create Store Form
+      <div>
+        <div className=' space-y-4 py-2 pb-4'>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name='name'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder='E-commerce' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className='pt-6 space-x-2 flex items-center justify-end w-full'>
+                <Button variant='outline' onClick={storeModal.onClose}>
+                  Cancel
+                </Button>
+                <Button type='submit'>Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
-  )
-}
+  );
+};
 
-export default StoreModal
+export default StoreModal;
