4b142777e0874dd70977ea061f9ed3c64214d5d6
rohitGupta2022 <pta.rohit28@gmail.com>
2023-08-13 18:06:09 +0530

    Add: Table Component
    @tanstack/react-table
    and date-fns

--- app/(dashboard)/[storeId]/(routes)/billboards/components/BillboardClient.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/BillboardClient.tsx
@@ -6,21 +6,30 @@ import { Separator } from '@/components/ui/separator'
 import { Plus } from 'lucide-react'
 import { useParams, useRouter } from 'next/navigation'
 import React from 'react'
+import { BillboardColumn, columns } from './Columns'
+import { DataTable } from '@/components/ui/DataTable'
 
-const BillboardClient = () => {
+interface BillboardClientProps {
+  data: BillboardColumn[]
+}
+
+const BillboardClient: React.FC<BillboardClientProps> = ({
+  data
+}) => {
   const router = useRouter();
   const params = useParams();
 
   return (
     <>
       <div className='flex items-center justify-between'>
-        <Heading title='Billboards (0)' description='Manage billboards for your store' />
+        <Heading title={`Billboards(${data.length})`} description='Manage billboards for your store' />
         <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
           <Plus className='mr-2 h-4 w-4' />
           Add New
         </Button>
       </div>
       <Separator />
+      <DataTable columns={columns} data={data} />
     </>
 
   )
--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { format } from 'date-fns'
 import BillboardClient from './components/BillboardClient'
+import prismadb from '@/lib/prismadb'
+import { BillboardColumn } from './components/Columns'
 
-const BillbordsPage = () => {
+const BillbordsPage = async ({ params }: { params: { storeId: string } }) => {
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, 'MMMM do, yyyy')
+  }))
+
   return (
     <div className='flex-col'>
       <div className='flex-1 space-y-4 p-8 pt-6'>
-        <BillboardClient />
+        <BillboardClient data={formattedBillboards} />
       </div>
     </div>
   )
