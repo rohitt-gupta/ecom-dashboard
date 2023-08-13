5566489329cff2a119e4804f78c142eb876f6ed7
rohitGupta2022 <pta.rohit28@gmail.com>
2023-08-13 16:20:43 +0530

    Add: API for Bibbloards.
    1 Create a billboard
    2. Get all billboards
    3. Get a single Billboard
    4. Edit a billboard
    5. delete a billboard

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillboardForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillboardForm.tsx
@@ -57,9 +57,16 @@ const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => {
   const onSubmit = async (data: BillboardFormValues) => {
     try {
       setLoading(true);
-      await axios.patch(`/api/stores/${params.storeId}`, data);
+      if (initialData) {
+        // if initial data present , we update/PATCH request API is called
+        await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data);
+      } else {
+        // if initial data not present, then we do POST / create billboard
+        await axios.post(`/api/${params.storeId}/billboards`, data);
+      }
       router.refresh();
-      toast.success("Store updated")
+      router.push(`/${params.storeId}/billboards`)
+      toast.success(toastMessage)
     } catch (error) {
       toast.error("Something went wrong")
     }
@@ -67,15 +74,14 @@ const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => {
 
   const onDelete = async () => {
     try {
-
       setLoading(true);
-      await axios.delete(`/api/stores/${params.storeId}`)
+      await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`)
       router.refresh();
       router.push("/")
-      toast.success('Store deleted.')
+      toast.success('Billbord deleted.')
 
     } catch (error) {
-      toast.error("Make sure you removed all the products and categories first.")
+      toast.error("Make sure you removed all the categories using this billboard.")
     } finally {
       setLoading(false)
       setOpen(false)
---
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -0,0 +1,107 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { billboardId: string } }
+) {
+  try {
+    if (!params.billboardId) { return new NextResponse("Billboard id is required", { status: 401 }) };
+
+    const billboard = await prismadb.billboard.findUnique({
+      where: {
+        id: params.billboardId,
+      }
+    });
+
+    return NextResponse.json(billboard);
+
+  } catch (error) {
+    console.log('[BILLBOARD_GET]', error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string, billboardId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { label, imageUrl } = body;
+
+    if (!userId) { return new NextResponse("Unauthenticateed", { status: 401 }) };
+
+    if (!label) { return new NextResponse("Label is Required", { status: 400 }) };
+
+    if (!imageUrl) { return new NextResponse("Image URL  is Required", { status: 400 }) };
+
+    if (!params.billboardId) { return new NextResponse("Billboard id is required", { status: 401 }) };
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId
+      }
+    })
+
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized', { status: 403 });
+    }
+
+    const billboard = await prismadb.billboard.updateMany({
+      data: {
+        label,
+        imageUrl,
+        storeId: params.storeId
+      }
+    });
+
+    return NextResponse.json(billboard);
+
+  } catch (error) {
+    console.log('[BILLBOARD_PATCH]', error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeId: string, billboardId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) { return new NextResponse("Unauthenticateed", { status: 401 }) };
+
+    if (!params.billboardId) { return new NextResponse("Billboard id is required", { status: 401 }) };
+
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId
+      }
+    })
+
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized', { status: 403 });
+    }
+
+    const billboard = await prismadb.billboard.deleteMany({
+      where: {
+        id: params.billboardId,
+      }
+    });
+
+    return NextResponse.json(billboard);
+
+  } catch (error) {
+    console.log('[BILLBOARD_DELETE]', error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
