1f842b1025e297b00f0263c8155a87671b63cf80
rohitGupta2022 <pta.rohit28@gmail.com>
2023-08-15 21:25:08 +0530

    Add: Products Schema and route

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -37,6 +37,11 @@ export function MainNav({ className, ...props }: React.HTMLAttributes<HTMLElemen
       active: pathname === `/${params.storeId}/colors`,
     },
     {
+      href: `/${params.storeId}/products`,
+      label: 'Products',
+      active: pathname === `/${params.storeId}/products`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: 'Settings',
       active: pathname === `/${params.storeId}/settings`,
