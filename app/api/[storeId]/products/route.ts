c3b8ec2dc48b3283a6c6398752a72045615a23ac
rohitGupta2022 <pta.rohit28@gmail.com>
2023-08-15 22:08:01 +0530

    Add: Product API

---
+++ app/api/[storeId]/products/route.ts
@@ -0,0 +1,71 @@
+// import { Billboard } from '@prisma/client';
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { label, imageUrl } = body
+
+
+    if (!userId) return new NextResponse('Unauthenticated User', { status: 401 });
+
+    if (!label) return new NextResponse('Label is required', { status: 400 });
+    if (!imageUrl) return new NextResponse('Image URL is required', { status: 400 });
+    if (!params.storeId) return new NextResponse('Store id is required', { status: 400 });
+
+    /**
+     * Authorization check
+     */
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId
+      }
+    })
+
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized', { status: 403 });
+    }
+
+
+    const billboard = await prismadb.billboard.create({
+      data: {
+        label,
+        imageUrl,
+        storeId: params.storeId,
+      }
+    });
+
+    return NextResponse.json(billboard);
+
+
+  } catch (error) {
+    console.log("[BILLBOARDS_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 })
+  }
+}
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) return new NextResponse('Store id is required', { status: 400 });
+    const billboard = await prismadb.billboard.findMany({
+      where: {
+        storeId: params.storeId
+      }
+    });
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARDS_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 })
+  }
+}
